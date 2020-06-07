c0e819237f6f40b33df4cd063c73c13d2af2238e
Alperen AYDIN <aydin.alperen.bugra@gmail.com>
2020-06-07 20:48:06 +0200

    Added the header for the PrimordialParticle

--- PPS/include/PPS/PrimordialParticle.hpp
+++ PPS/include/PPS/PrimordialParticle.hpp
@@ -1,1 +1,47 @@
 #pragma once
+
+#include <opencv2/opencv.hpp>
+
+#include <utility>
+
+
+namespace PPS
+{
+  // Forward declarations
+  class PrimordialParticleSystem; 
+  
+  class PrimordialParticle
+  {
+  public:
+    // Standard issue constructor
+    PrimordialParticle(float iX, float iY, float iPhi,
+                       float v, float alpha, float beta, float radius);
+
+    // Update state
+    void update();
+
+    // Reset the counters
+    void resetCounters(); 
+
+    // Draw on the canvas
+    void draw(cv::Mat& ioCanvas, cv::Scalar iColor);
+
+    // Simulate the interaction between points
+    static void ParticleInteraction(PrimordialParticle& a, PrimordialParticle& b);
+
+  private:
+    // State
+    float x, y;
+    float phi; // In degrees
+
+    // Parameters
+    float v;
+    float alpha, beta;
+    float radius;
+
+    // Counters
+    int N;
+    int RL; // R-L 
+    
+  };
+} // namespace PPS
