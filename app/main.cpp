b482bdcf5dd3dbfa5e54c14ccfa3d6c79c24d89e
Alperen AYDIN <aydin.alperen.bugra@gmail.com>
2020-06-09 21:20:41 +0200

    Added a fading effect

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <PPS/PrimordialParticleSystem.hpp>
+#include <opencv2/core.hpp>
 #include <opencv2/core/hal/interface.h>
 #include <opencv2/core/types.hpp>
 #include <opencv2/highgui.hpp>
@@ -9,20 +10,20 @@ int main()
 {
   int width = 1000;
   int height = 1000;
-  PPS::PrimordialParticleSystem aSystem(100,
+  PPS::PrimordialParticleSystem aSystem(500,
                                         width, height,
-                                        6, 0, -6, 55);
+                                        15, 120, -60, 55);
   cv::namedWindow("canvas", cv::WINDOW_AUTOSIZE);
-  cv::Mat aCanvas(height, width, CV_8UC3, cv::Scalar(255,255,255));
+  cv::Mat aCanvas(height, width, CV_8UC4, cv::Scalar(255,255,255));
 
   while(1)
     {
-      aCanvas = cv::Scalar(255,255,255);
+      aCanvas = aCanvas + cv::Scalar(50,50,50);
       aSystem.update();
       aSystem.draw(aCanvas);
 
       cv::imshow("canvas", aCanvas);
-      char c = (char)cv::waitKey(5);
+      char c = (char)cv::waitKey(20);
       if(c==27)
         break;
     }
